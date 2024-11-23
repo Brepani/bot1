03cd66f693aa90c1aab554380cafdb03104b3ed2
Brepani <brendapaniagua.r@gmail.com>
2024-11-23 13:46:45 -0600

    Update app.py

--- app.py
+++ app.py
@@ -5,7 +5,7 @@ st.balloons()
 # Show title and description.
 st.title("💬 Panibot")
 st.write(
-   "This is a simple chatbot that uses OpenAI's GPT-3.5 model to generate responses. "
+   "Este es un chatbot de prueba para el diplomado Inteligencia Internacional Aplicada desde la Ética y El Humanismo. "
    "To use this app, you need to provide an OpenAI API key, which you can get [here](https://platform.openai.com/account/api-keys). "
    "You can also learn how to build this app step by step by [following our tutorial](https://docs.streamlit.io/develop/tutorials/llms/build-conversational-apps)."
 )
